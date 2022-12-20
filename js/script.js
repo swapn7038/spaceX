6d87dcd33fd5d79abda1603cb4ae626e59a27ab6
Swapnil Shelke <swapnilbabanshelke@gmail.com>
2022-12-20 10:52:26 +0530

    Completed...

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const btn = document.getElementById("menu-btn");
+const overlay = document.getElementById("overlay");
+
+btn.addEventListener("click", navToggle);
+
+function navToggle() {
+  btn.classList.toggle("open");
+  overlay.classList.toggle("overlay-show");
+}
+
+// onw of the way
+// btn.addEventListener("click", () => {
+//   btn.classList.toggle("open");
+// });
